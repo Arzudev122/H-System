af0cce4644423d7c7358f4f78b0f482fc28280ad
Arzudev122 <devkotaarzu122@gmail.com>
2025-11-09 13:30:56 +0545

    Updated login form styling

--- src/component/dashboard/dashboard.jsx
+++ src/component/dashboard/dashboard.jsx
@@ -1,19 +1,99 @@
 import React from 'react'
 import Navbar from '../shared/navbar/navbar'
 import Sidebar from '../shared/sidebar/sidebar'
+import { Bed, CalendarCheck2, Ellipsis, Users } from 'lucide-react'
 
 const Dashboard = () => {
+   const cards=[
+    {
+      tittle:"Total Patients",
+      subtittle:"150 more than Yesterday",
+      iconLeft:<Users/>,
+      bg: 'bg-violet-200',
+    },
+     {
+      tittle:"Appointments",
+      subtittle:"15 more than Yesterday",
+      iconLeft:<CalendarCheck2/>,
+      bg: 'bg-gray-300',
+    },
+     {
+      tittle:"Bed Room",
+      subtittle:"260 more than Yesterday",
+      iconLeft:<Bed/>,
+      bg: 'bg-yellow-200',
+    },
+     {
+      tittle:"Total Invoice",
+      subtittle:"1050 more than Yesterday",
+      iconLeft:<Users/>,
+      bg: 'bg-violet-200',
+    },
+   ]
+
   return (
     <div className='flex h-screen'>
-    <Sidebar />
+      <Sidebar />
 
-    <div className='flex flex-col  flex-1'>
-      <Navbar/>
+      <div className='flex flex-col  flex-1'>
+        <Navbar />
 
-      <main className='p-4 overflow-auto flex-1'>
+        <main className='p-4 overflow-auto flex-1'>
+
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6'>
+            {cards.map((card,i)=>(
+              <div
+              key={i}
+              className="">
+            </div>))}
+
+          </div>
+
+          {/* <div className='min-h-screen shadow-xl rounded-4xl bg-violet-50'>
+            <div className='flex justify-between gap-2 p-5'>
+              <div className='flex justify-between bg-violet-200 shadow-md rounded-3xl h-57 w-86'>
+                <span className='m-4 flex justify-center items-center w-12 h-12 rounded-full bg-gray-100 hover:bg-white'>
+                  <Users className=' h-6 w-6' />
+                </span>
+                 <h2 className='flex justify-center items-center'>Total Patients</h2>
+                <p>150 more than Yesterday</p>
+                <span className='m-4 flex justify-center items-center w-12 h-12 rounded-full bg-violet-100 hover:bg-violet-300'>
+                  <Ellipsis className='h-6 w-6' />
+                </span>
+               
+              </div>
+
+              <div className='flex justify-between bg-gray-300 shadow-md rounded-3xl h-57 w-86'>
+                <span className='m-4 flex justify-center items-center w-12 h-12 rounded-full bg-gray-100 hover:bg-gray-400'>
+                  <CalendarCheck2 className='h-6 w-6' />
+                </span>
+                <span className='m-4 flex justify-center items-center w-12 h-12 rounded-full bg-gray-200 hover:bg-gray-400'>
+                  <Ellipsis className='h-6 w-6' />
+                </span>
+              </div>
+              <div className='flex justify-between bg-yellow-200 shadow-md rounded-3xl h-57 w-86'>
+                <span className='m-4 flex justify-center items-center w-12 h-12 rounded-full bg-black '>
+                  <Bed className='h-6 w-6 text-white' />
+                </span>
+                <span className='m-4 flex justify-center items-center w-12 h-12 rounded-full bg-yellow-100 hover:bg-white'>
+                  <Ellipsis className='h-6 w-6' />
+                </span>
+              </div>
+              <div className='flex justify-between bg-gray-200 shadow-md rounded-3xl h-57 w-86'>
+                <span className='m-4 flex justify-center items-center w-12 h-12 rounded-full bg-gray-100 hover:bg-white'>
+                  <Users className='h-6 w-6' />
+                </span>
+                <span className='m-4 flex justify-center items-center w-12 h-12 rounded-full bg-gray-100 hover:bg-white'>
+                  <Ellipsis className='h-6 w-6' />
+                </span>
+              </div>
+            </div>
+
+          </div> */}
+
 
         </main>
-    </div>
+      </div>
     </div>
   )
 }
--- src/component/shared/navbar/navbar.jsx
+++ src/component/shared/navbar/navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { useState } from 'react'
-import {Search ,Mails, Bell} from "lucide-react"
+import {Search ,Mails, Bell, Settings, ChevronDown } from "lucide-react"
 
 
 const Navbar = () => {
@@ -15,8 +15,8 @@ const Navbar = () => {
 
   return (
     <nav className='flex items-center gap-6 px-6 py-4 bg-white rounded-2xl'>
-    <div className='relative w-1/2'>
-       <Search className='absolute left-2 top-1/2 -translate-y-1/2 h-5 w-5'/> 
+    <div className='relative w-1/2 mr-3'>
+       <Search className='absolute left-2 top-1/2 -translate-y-1/2 h-5 w-5 '/> 
        <input
         type="search"
         placeholder=" Search here..."
@@ -26,9 +26,23 @@ const Navbar = () => {
         /> 
          </div>
            
-           <div className='flex space-x-8 p-6'>
-         <Mails className='text-black h-5 w-5'/>
-         <Bell className='text-black h-5 w-5'/>
+           <div className='flex space-x-8 p-4 border-x-4 border-gray-100 py-1 mx-3'>
+         <span className='flex justify-center items-center rounded-full h-10 w-10 bg-gray-100 hover:bg-gray-300'><Mails className='text-black h-5 w-5'/></span> 
+         <span className='flex justify-center items-center rounded-full h-10 w-10 bg-gray-100 hover:bg-gray-300'> <Bell className='text-black h-5 w-5'/></span>
+          <span className='flex justify-center items-center rounded-full h-10 w-10 bg-gray-100 hover:bg-gray-300'><Settings className='text-black h-5 w-5'/> </span>
+            </div>
+
+            <div className='flex justify-between items-center ml-6 gap-4'>
+                <img 
+                src="./src/assets/img/profile.jpg"
+                alt="profile picture"
+                className='h-14 w-14 rounded-full '
+                />
+                <span className='ml-3'>
+                  <h2 className='font-bold text-lg'>Jackie Chain</h2>
+                  <p className='font-medium'>Super admin</p>
+                </span>
+                <ChevronDown/>
             </div>
 
      </nav>
