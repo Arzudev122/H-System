d497b1ad507a154fd37a4b81d94e8f4d824176f4
Arzudev122 <devkotaarzu122@gmail.com>
2025-12-12 23:11:54 +0545

    New start in doctor

---
+++ src/component/Calendar/calendar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Calendar = () => {
+  return (
+    <>
+    <h1>Calander</h1>
+    </>
+  )
+}
+
+export default Calendar
---
+++ src/component/Doctor/doctorSchedule.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DoctorSchedule = () => {
+  return (
+    <>
+   <h1>DoctorSchedule</h1>
+    </>
+  )
+}
+
+export default DoctorSchedule
